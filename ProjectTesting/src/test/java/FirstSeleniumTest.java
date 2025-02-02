951ab0679d31944244853e3165fe41bb54eb967e
viktarilukovich <vata66@tut.by>
2025-02-02 17:23:35 +0300

    Selenium tests: following links, using the Actions class

--- ProjectTesting/src/test/java/FirstSeleniumTest.java
+++ ProjectTesting/src/test/java/FirstSeleniumTest.java
@@ -2,9 +2,9 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.safari.SafariDriver;
+import org.openqa.selenium.safari.SafariOptions;
 import org.testng.annotations.Test;
 
-import java.lang.reflect.Array;
 import java.util.ArrayList;
 
 public class FirstSeleniumTest {
@@ -13,7 +13,16 @@ public class FirstSeleniumTest {
         WebDriver driver = new SafariDriver();
         driver.get("https://the-internet.herokuapp.com");
         ArrayList<WebElement> element = new ArrayList<>(driver.findElements(By.tagName("a")));
-        element.get(10).click();
+        element.get(9).click();
+        driver.quit();
+    }
+    @Test
+    public void secondTest() {
+        SafariOptions options = new SafariOptions();
+        options.setAcceptInsecureCerts(true);
+        WebDriver driver = new SafariDriver(options);
+        driver.get("https://litecart.stqa.ru/en/");
+        driver.manage().window().maximize();
         driver.quit();
     }
 }
